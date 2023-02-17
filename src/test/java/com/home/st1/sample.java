7d1ec34ec93ac97c90c6a0237fc3de3804664072
LYS-1 <skyduq123@gmail.com>
2023-02-17 11:53:19 +0900

    02_17

--- src/test/java/com/home/st1/sample.java
+++ src/test/java/com/home/st1/sample.java
@@ -1,14 +1,14 @@
 package com.home.st1;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
-public class sample {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-
-}
+//import static org.junit.Assert.*;
+//
+//import org.junit.Test;
+//
+//public class sample {
+//
+//	@Test
+//	public void test() {
+//		fail("Not yet implemented");
+//	}
+//
+//}
